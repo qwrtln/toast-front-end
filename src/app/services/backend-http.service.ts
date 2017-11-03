65583d52b5af11a4ac418f5910ca5d73aa8376ed
Marcin Szewczyk <marcin91.szewczyk@gmail.com>
2017-11-03 18:18:13 +0100

    Use separate environments for each build mode

--- src/app/services/backend-http.service.ts
+++ src/app/services/backend-http.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpService } from './http.service';
 
 declare const require: any;
-const config = require('../../config.json');
+const config = require('../../environments/environment').environment;
 
 // The purpose of this class is to provide an abstract service,
-// which loads backend URL from the config.json file and then
+// which loads backend URL from the enivronments.ts file and then
 // shares it with all its children.
 @Injectable()
 export abstract class BackendHttpService extends HttpService {
